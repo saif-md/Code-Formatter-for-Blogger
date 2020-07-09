1aa638e5c5f2a0ad17204bcfdc1932eb3064f223
saif-md <64372765+saif-md@users.noreply.github.com>
2020-07-10 00:53:57 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from browser import document
+
+def Formatter(event):
+  code = document['input-area'].value
+  FC = ""
+  
+  for i in code:
+    if i == "<":
+      FC += "&lt;"
+    elif i == ">":
+      FC += "&gt;"
+    elif i == "&":
+      FC += "&amp;"
+    elif i == "\n":
+      FC += "<br>"
+    else:
+      FC += i
+  
+  document['output-area'].text = FC
+  
+document['submit-button'].bind('click', Formatter)
+
+def copier(event):
+  FormattedElement = document['output-area']
+  FormattedElement.select()
+  document.execCommand('copy')
+  
+document['output-area'].bind('focus', copier)
